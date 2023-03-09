494d74a27d6b84ddf167ab77d63f0e89dbe6e7af
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-03-09 17:12:50 +0200

    Querysets

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Base, Agent
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Base)
+admin.site.register(Agent)
--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # installed apps 
+    # My apps 
     'base',
 ]
 
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'base.User'
