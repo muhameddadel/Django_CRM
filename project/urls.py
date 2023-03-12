35ceba3d62983ff4f76747c66b59709a472564f3
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-03-12 15:21:25 +0200

    static files

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from base.views import LandingPageView
 
@@ -22,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page' ),
     path('base/', include('base.urls',namespace= 'base')),
-]
+    ] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
