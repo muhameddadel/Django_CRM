75d054abfceec8e672b4b582f0f2f055af54d1fc
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-03-21 14:02:47 +0200

    mofiy model

--- base/models.py
+++ base/models.py
@@ -23,7 +23,11 @@ class Lead(models.Model):
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null= True, blank= True, on_delete= models.SET_NULL)
     category = models.ForeignKey('Category', related_name='leads', null= True, blank= True, on_delete=models.SET_NULL)
-
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add= True)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
     
